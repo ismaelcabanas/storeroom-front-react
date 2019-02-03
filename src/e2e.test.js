7655e5c59f52d141e3aaef1e40c838aa752a885d
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-02-03 23:49:31 +0100

    First e2e test

---
+++ src/e2e.test.js
@@ -0,0 +1,26 @@
+import puppeteer from 'puppeteer'
+
+const appUrlBase = 'http://localhost:3000'
+
+let browser
+let page
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({})
+  page = await browser.newPage()
+})
+
+describe('Products', () => {
+  test('Heading', async () => {
+    await page.goto(`${appUrlBase}/`)
+    await page.waitForSelector('h1')
+    const result = await page.evaluate(() => {
+      return document.querySelector('h1').innerText
+    })
+    expect(result).toEqual('Storeroom Products')
+  })
+})
+
+afterAll(() => {
+  browser.close()
+})
