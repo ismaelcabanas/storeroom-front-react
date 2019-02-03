1a1e7312f62cb85b7b6ac0912061624286ea7131
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-02-03 23:59:41 +0100

    Product list e2e test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Storeroom Products</h1>
+        <div className="products">
+          <div className="product">
+            <h2 className="title">Manzana</h2>
+          </div>
+          <div className="product">
+            <h2 className="title">Leche</h2>
+          </div>
+        </div>
       </div>
     );
   }
