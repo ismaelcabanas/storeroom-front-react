84ae854de17c6eeaeeacfaa48ec0d43b4b07820f
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-02-04 08:59:31 +0100

    Refactoring
    I refactor the code to use a function to generate the product list.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  renderProducts(products) {
+    return(
+      <div className="products">
+      {
+        products.map((product, index) => {
+          return(
+            <div key={index} className="product">
+              <h2 className="title">{product.name}</h2>
+            </div>
+          )
+        })
+      }
+      </div>
+    )
+  }
+
   render() {
+    const products = [{"name": "Manzana"}, {"name": "Leche"}]
+
     return (
       <div className="App">
         <h1>Storeroom Products</h1>
-        <div className="products">
-          <div className="product">
-            <h2 className="title">Manzana</h2>
-          </div>
-          <div className="product">
-            <h2 className="title">Leche</h2>
-          </div>
-        </div>
+        {this.renderProducts(products)}
       </div>
     );
   }
