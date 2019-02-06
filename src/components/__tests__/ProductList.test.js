2c74cad87aec39d0b07d841c9b1dccd43d5d7c23
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-02-06 13:25:34 +0100

    New unit tests with happy path in ProductList component

--- src/components/__tests__/ProductList.test.js
+++ src/components/__tests__/ProductList.test.js
@@ -30,4 +30,17 @@ describe("ProductList", () => {
 
         expect(wrapper.find('.error').length).toEqual(1)
     })
+
+    it("Shows a list of products", () => {
+        const props = {
+            products: [
+                {"id": "6ee88e09-8b8a-44c4-b8b3-9ba8e759284d", "name": "Manzana", "storeroomId": "dc3143d7-7731-4532-a5e6-b35e7149350f"},
+                {"id": "56259dc5-c7b4-4489-a9dd-7cf56c1134e6", "name": "Leche", "storeroomId": "dc3143d7-7731-4532-a5e6-b35e7149350f"}
+            ]
+        }
+
+        const wrapper = shallow(<ProductList {... props} />)
+
+        expect(wrapper.find('.product .title').length).toEqual(2)
+    })
 })
