74d7755f6318231e9a7bd6550ce7fe30c2e1bf3d
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-02-07 00:02:50 +0100

    Refactor

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 
 class ProductList extends Component {
-  constructor(props) {
-    super(props)    
-  }
   
   render () {        
     if(this.props.loading) {
