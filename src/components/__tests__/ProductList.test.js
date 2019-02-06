95501aca4e6aabd71748d884ff07a25108344d84
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-02-06 12:52:05 +0100

    ProductList component error bar
    When the ProductList component has some error it will show a error message.

--- src/components/__tests__/ProductList.test.js
+++ src/components/__tests__/ProductList.test.js
@@ -9,12 +9,25 @@ Enzyme.configure({adapter: new Adapter()});
 describe("ProductList", () => {
     it('Shows a loading bar when is loading', () => {
         const props = {
-            loading: true,
+            loading: true, 
             products: []
         }
         
         const wrapper = shallow(<ProductList {... props} />)
 
         expect(wrapper.find('.loading').length).toEqual(1)
+    })
+
+    it("Shows a error bar when error happens", () => {
+        const props = {
+            error: {
+                "message": "Something went wrong"
+            },
+            products: []
+        }
+
+        const wrapper = shallow(<ProductList {... props} />)
+
+        expect(wrapper.find('.error').length).toEqual(1)
     })
 })
