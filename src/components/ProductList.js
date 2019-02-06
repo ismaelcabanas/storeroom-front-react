a84413c8475c912995e0b82e23b1a992c7647c52
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-02-06 12:33:31 +0100

    ProductList component loading bar
    When the ProductList component is not ready it will show a loading bar.

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 
 class ProductList extends Component {
-    render () {        
-        return (
-            <div className="products">
-            {
-              this.props.products.map((product, index) => {
-                return (
-                  <div key={index} className="product">
-                    <h2 className="title">{product.name}</h2>
-                  </div>
-                )
-              })
-            }
+  constructor(props) {
+    super(props)    
+  }
+  
+  render () {        
+    if(this.props.loading) {
+      return (<div className="loading" />)
+    }
+    return (      
+      <div className="products">
+      {
+        this.props.products.map((product, index) => {
+          return (
+            <div key={index} className="product">
+              <h2 className="title">{product.name}</h2>
             </div>
           )
-    }    
+        })
+      }
+      </div>
+    )
+  }    
 }
 export default ProductList
