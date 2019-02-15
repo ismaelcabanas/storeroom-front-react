a26b2b739ac10c210a41d420f53878b71401c6cf
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-02-15 08:44:27 +0100

    Add routing capabilities to the app
    So the whole application can share global Router configuration.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import ProductListContainer from './components/ProductListContainer';
+import ProductDetailContainer from './components/ProductDetailContainer';
+import {Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Storeroom Products</h1>
-        <ProductListContainer />
+        <main>
+          <Route exact path="/" component={ProductListContainer} />
+          <Route path="/products/:id" component={ProductDetailContainer} />
+        </main>
       </div>
     );
   }
