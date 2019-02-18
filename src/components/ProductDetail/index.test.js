74f27edb924f94a171f6c4064cddee2c4ba64f68
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-02-18 10:17:06 +0100

    Refactor file structure following a React pattern

--- src/components/ProductDetail/index.test.js
+++ src/components/ProductDetail/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import ProductDetail from '../ProductDetail';
+import ProductDetail from '.';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
--- src/containers/ProductDetailContainer.js
+++ src/containers/ProductDetailContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import ProductDetail from './ProductDetail';
+import ProductDetail from '../components/ProductDetail/index';
 
 class ProductDetailContainer extends Component {
     constructor(props) {
