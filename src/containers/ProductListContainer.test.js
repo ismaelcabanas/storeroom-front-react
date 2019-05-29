1015f26a0952ea0f8869a003f53fc5d211d9f4f0
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-05-29 18:09:45 +0200

    Failed ProductListContainer test

---
+++ src/containers/ProductListContainer.test.js
@@ -0,0 +1,14 @@
+import {fetchProducts} from './actions';
+
+describe('ProductListContainer related actions', () => {
+    it('Set search keyword', () => {
+        const term = ''
+        const expected = {
+            type: 'SET_TERM_SEARCH',
+            term
+        }
+        const action = setSearchTerm(term)
+
+        expect(action).toEqual(expected)
+    })
+})
