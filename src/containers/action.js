f5caa7dee37ef9d44c31dc35d8530372ce42ef40
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-06-04 17:46:51 +0200

    fetchProducts using axios

--- src/containers/action.js
+++ src/containers/action.js
@@ -1,3 +1,14 @@
+import axios from 'axios'
+
 export const setSearchTerm = (term) => {
     return {type: 'SET_SEARCH_TERM', payload: {term}}
 }
+
+export const fetchProducts = () => {
+    return (dispatch) => {
+      dispatch({type: 'FETCH_PRODUCTS_PENDING'})
+      return axios.get(`http://localhost:8080/storeroom/1/products`).then((res) => {
+        dispatch({type: 'FETCH_PRODUCTS_SUCCESS', payload: res.data})
+      })
+    }
+  }
