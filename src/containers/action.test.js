ef3da1ce421c8a70f32da8e0cb6034bb4204af62
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-06-04 09:09:03 +0200

    Action search term

--- src/containers/action.test.js
+++ src/containers/action.test.js
@@ -1,11 +1,4 @@
-import {setSearchTerm, fetchProducts} from './action'
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-
-import axios from 'axios'
-
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
+import {setSearchTerm} from './action'
 
 describe('ProductListContainer related actions', () => {
     it('Set search keyword', () => {
@@ -18,37 +11,5 @@ describe('ProductListContainer related actions', () => {
       expect(action).toEqual(expected)
     })
   
-    describe('Fetching data from remote', () => {
-        it('Fetch data successfully', () => {
-            const products = [
-                {id: 1, name: 'Manzana'},
-                {id: 2, name: 'Leche'}
-            ]
-            axios.get = jest.fn().mockImplementation(() => Promise.resolve({data: products}))
-        
-            const expectedActions = [
-                {type: 'FETCH_PRODUCTS_PENDING'},
-                {type: 'FETCH_PRODUCTS_SUCCESS', payload: products}
-            ]
-            const store = mockStore({products: []})
-        
-            return store.dispatch(fetchProducts('')).then(() => {
-                expect(store.getActions()).toEqual(expectedActions)
-            })            
-        })
 
-        it('Fetch data with error', () => {
-            axios.get = jest.fn().mockImplementation(() => Promise.reject({message: 'Something went wrong'}))
-      
-            const expectedActions = [
-              { type: 'FETCH_BOOKS_PENDING'},
-              { type: 'FETCH_BOOKS_FAILED', payload: {message: 'Something went wrong' }}
-            ]
-            const store = mockStore({books: [], search: {term: '' }})
-      
-            return store.dispatch(fetchProducts()).then(() => {
-              expect(store.getActions()).toEqual(expectedActions)
-            })
-        })
-    })
 })
