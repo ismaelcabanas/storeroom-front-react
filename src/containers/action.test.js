d0af1bff65a73ef384dbc74aa7a23a6b8136aca0
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-06-04 09:19:16 +0200

    Action async
    Para async actions es un poco más ío. Hay que configurar redu-thunk para crear un mock store. En este caso esperamos que la acción fetchProducts puedacrear dos acciones: una para indicar que la petición se está enviao, y otra para indicar que la respuesta llegÃ. Por ello, usamos jest.fn().mockImplementation() para crear un stub con la petición desde axios.

--- src/containers/action.test.js
+++ src/containers/action.test.js
@@ -1,4 +1,12 @@
-import {setSearchTerm} from './action'
+import {setSearchTerm, fetchProducts} from './action'
+
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+import axios from 'axios'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
 
 describe('ProductListContainer related actions', () => {
     it('Set search keyword', () => {
@@ -11,5 +19,24 @@ describe('ProductListContainer related actions', () => {
       expect(action).toEqual(expected)
     })
   
+    describe('Fetching data from remote', () => {
+        it('Fetch data successfully', () => {
+            const products = [
+              {id: 1, name: 'Manzanas'},
+              {id: 2, name: 'Leche'}
+            ]
+            axios.get = jest.fn().mockImplementation(() => Promise.resolve({data: products}))
+      
+            const expectedActions = [
+              { type: 'FETCH_PRODUCTS_PENDING'},
+              { type: 'FETCH_PRODUCTS_SUCCESS', payload: products }
+            ]
+            const store = mockStore({products: [], search: { term: '' } })
+      
+            return store.dispatch(fetchProducts()).then(() => {
+              expect(store.getActions()).toEqual(expectedActions)
+            })
+          })
+    })
 
 })
